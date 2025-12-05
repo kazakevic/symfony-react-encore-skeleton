586f5f50c0a8f613627a6dd9d1d010e3c0a223c1
kazakevic <darjus.kazakevic@gmail.com>
2025-12-05 20:31:09 +0200

    chore: init doctrine, postgres

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -7,4 +7,7 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
